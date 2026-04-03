6861142d9e8e21dde4c5e41f9daf1ae31d825d3d
as-soso-as2011-design <as-soso-as2011@hotmail.com>
2026-04-03 16:50:52 +0300

    Delete mobile-script.js

--- mobile-script.js
+++
@@ -1,8 +0,0 @@
-
-(function(){
-  const links = document.querySelectorAll('.bottom-nav a');
-  const current = location.pathname.split('/').pop();
-  links.forEach(a=>{
-    if(a.getAttribute('href') === current){ a.classList.add('active'); }
-  });
-})();
